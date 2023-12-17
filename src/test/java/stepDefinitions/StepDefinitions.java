3ee0654909ecce708b735a13c091673d95e6a85a
mustafa <mustafa.bykgz@gmail.com>
2023-12-17 14:31:00 +0300

    v10

--- src/test/java/stepDefinitions/StepDefinitions.java
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -3,46 +3,38 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.messages.types.Hook;
 import org.junit.Assert;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.AmazonPage;
 import utilities.ConfigurationReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
 
-import java.time.Duration;
+
 
 
 public class StepDefinitions {
     AmazonPage amazon = new AmazonPage();
-    Actions actions = new Actions(Driver.getDriver());
-    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+
 
 
     @Given("Kullanici Amazon anasayfasina gider")
     public void kullanici_amazon_anasayfasina_gider() {
 
         Driver.getDriver().get(ConfigurationReader.getProperty("amazonUrl"));
-        wait.until(ExpectedConditions.visibilityOf(amazon.cookiesAccept));
         ReusableMethods.waitFor(2);
     }
 
     @When("Giris yap sekmesine tiklar")
     public void giris_yap_sekmesine_tiklar() {
         ReusableMethods.waitFor(2);
-       amazon.accountList.click();
+        amazon.accountList.click();
     }
 
     @Then("Gecerli kullanici adini girer")
     public void gecerli_kullanici_adini_girer() {
         ReusableMethods.waitFor(2);
-        amazon.emailBox.click();
-        actions.sendKeys(amazon.emailBox, "mustafa_bykgz@hotmail.com").perform();
+        amazon.emailBox.sendKeys("mustafa_bykgz@hotmail.com");
 
     }
 
@@ -57,7 +49,7 @@ public class StepDefinitions {
     @Then("Gecerli password bilgisini girer")
     public void gecerli_password_bilgisini_girer() {
         ReusableMethods.waitFor(2);
-        actions.sendKeys(amazon.passwordBox, "060512020mM").perform();
+        amazon.passwordBox.sendKeys("060512020mM");
 
     }
 
@@ -70,8 +62,8 @@ public class StepDefinitions {
     @Then("Kullanici arama kutusuna herhangi bir gecerli veri girisi yapar")
     public void kullanici_arama_kutusuna_herhangi_bir_gecerli_veri_girisi_yapar() {
         ReusableMethods.waitFor(2);
-        actions.sendKeys(amazon.searchBox, "Java")
-                .sendKeys(Keys.ENTER).perform();
+        amazon.searchBox.sendKeys("Java", Keys.ENTER);
+
 
     }
 
@@ -89,7 +81,7 @@ public class StepDefinitions {
     @Then("Kullanici fiyati en dusuk olan urune tiklar")
     public void kullanici_fiyati_en_dusuk_olan_urune_tiklar() {
         ReusableMethods.waitFor(2);
-        ReusableMethods.waitForVisibility(amazon.product, 5).click();
+        amazon.product.click();
 
     }
 
